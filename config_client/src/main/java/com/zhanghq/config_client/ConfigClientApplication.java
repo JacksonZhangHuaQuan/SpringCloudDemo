29c819dabdfb2bd04d2faba34414aadfaefb1727
zhanghq <zhanghq@mail.taiji.com.cn>
2019-09-06 14:13:28 +0800

    1.3.2 config配置中心高可用性

--- config_client/src/main/java/com/zhanghq/config_client/ConfigClientApplication.java
+++ config_client/src/main/java/com/zhanghq/config_client/ConfigClientApplication.java
@@ -2,9 +2,12 @@ package com.zhanghq.config_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
--- config_service/src/main/java/com/zhanghq/config_service/ConfigServiceApplication.java
+++ config_service/src/main/java/com/zhanghq/config_service/ConfigServiceApplication.java
@@ -3,9 +3,11 @@ package com.zhanghq.config_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
